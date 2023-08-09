c0c6fc786ed32daec7c6e688194c9bc8f2369d24
Nevzat Anıl Talay <56592113+NevzatTalay@users.noreply.github.com>
2023-08-09 15:34:44 +0300

    Add files via upload

---
+++ movies/12.sql
@@ -0,0 +1,4 @@
+--SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp" OR name = "Helena Bonham Carter");
+SELECT "title" FROM "movies"
+WHERE "id" IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp"))
+AND "id" IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Helena Bonham Carter"));
